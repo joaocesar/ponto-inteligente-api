b9724fb4a98b980e13d83ea3f80814b31537dfc4
Joao Cesar Pereira <joao.cesar@gmail.com>
2019-08-29 11:08:23 -0300

    Adicionado serviço para empresa e seu respectivo test

---
+++ src/main/java/br/inf/jcp/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package br.inf.jcp.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import br.inf.jcp.pontointeligente.api.entities.Empresa;
+
+/**
+ * @author joaocesar
+ *
+ */
+public interface EmpresaService {
+
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+	
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 * 
+	 * @param empresa
+	 * @return Empresa
+	 */
+	Empresa persistir(Empresa empresa);
+	
+}
