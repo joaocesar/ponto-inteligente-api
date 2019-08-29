b35f2c0c2866e1c29a85856cde07e7069aae210e
Joao Cesar Pereira <joao.cesar@gmail.com>
2019-08-29 11:30:43 -0300

    Adicionado o serviço de funcionário

--- src/main/java/br/inf/jcp/pontointeligente/api/repositories/FuncionarioRepository.java
+++ src/main/java/br/inf/jcp/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -3,6 +3,8 @@
  */
 package br.inf.jcp.pontointeligente.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,8 @@ import br.inf.jcp.pontointeligente.api.entities.Funcionario;
  */
 @Transactional(readOnly = true)
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+	
+	Optional<Funcionario> findOne(Long id);
 
 	Funcionario findByCpf(String cpf);
 	
