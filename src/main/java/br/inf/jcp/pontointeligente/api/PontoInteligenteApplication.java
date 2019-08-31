b4ecfb0739b38f35dca358933a8048309343a133
Joao Cesar Pereira <joao.cesar@gmail.com>
2019-08-31 09:28:34 -0300

    Adicionados CadastroPJ e alterado a configuração padrão de segurança do Spring Boot

--- src/main/java/br/inf/jcp/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/inf/jcp/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,12 +2,21 @@ package br.inf.jcp.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+//@Configuration
+//@EnableWebSecurity
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+    }
 }
